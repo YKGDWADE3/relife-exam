05eaa7d6f172df940897d44399d1471e738a0a8b
YKGDWADE3 <superwadeg3@gmail.com>
2018-09-27 10:47:57 +0800

    [Kaiguang][Relife:Step1]finish setp1

--- src/main/java/com/tw/relife/RelifeApp.java
+++ src/main/java/com/tw/relife/RelifeApp.java
@@ -1,18 +1,26 @@
 package com.tw.relife;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class RelifeApp implements RelifeAppHandler {
     private final RelifeAppHandler handler;
 
     public RelifeApp(RelifeAppHandler handler) {
         // TODO: You can start here
-        throw new NotImplementedException();
+        if (handler == null) {
+            throw new IllegalArgumentException();
+        }
+        this.handler = handler;
     }
 
     @Override
     public RelifeResponse process(RelifeRequest request) {
-        // TODO: You can start here
-        throw new NotImplementedException();
+        RelifeResponse response = new RelifeResponse(200);
+        try {
+            response = handler.process(request);
+        }catch (Exception ex){
+            response = new RelifeResponse(500);
+        }finally {
+            return response;
+        }
+
     }
 }
